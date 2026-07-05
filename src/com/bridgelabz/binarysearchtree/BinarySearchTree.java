75441a1a29212ac7e4100b6f5c69ae5a27db17a0
Pavi512 <gondipavithra@gmail.com>
2026-07-05 21:04:17 +0530

    [Pavithra] : Implemented UC2 of binarysearch tree of creating BST and the size function to find whether all the nodes are added or not

--- src/com/bridgelabz/binarysearchtree/BinarySearchTree.java
+++ src/com/bridgelabz/binarysearchtree/BinarySearchTree.java
@@ -45,6 +45,14 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
     }
 
+    // Size of Tree
+    public int size(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + size(root.left) + size(root.right);
+    }
+
     //Main method
     public static void main(String[] args) {
 
@@ -53,8 +61,22 @@ public class BinarySearchTree<T extends Comparable<T>> {
         bst.add(56);
         bst.add(30);
         bst.add(70);
+        bst.add(22);
+        bst.add(40);
+        bst.add(60);
+        bst.add(95);
+        bst.add(11);
+        bst.add(65);
+        bst.add(3);
+        bst.add(16);
+        bst.add(63);
+        bst.add(67);
 
         System.out.println("Inorder Traversal:");
         bst.inorder(bst.root);
+
+        System.out.println("\n");
+
+        System.out.println("Size of Binary Tree : " + bst.size(bst.root));
     }
 }
